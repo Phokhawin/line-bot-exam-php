020a430438e721e8ed9c8e71bcd6b0297d34b409
Phokhawin <devilzenki@gmail.com>
2018-06-14 13:15:16 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YMWPbaM2Yglw+Wsywhnqa5yPVJeNyzX+cNUzU6fvofcMTqE+eGAHeJvNZgiD6GY5AKmqFYb7tAxDil4U6kBYFtb+qbpSX6LGIx1koTV149sVgQgU1V6XCpNwwBiLhCaxhEv7bsGHQOOkPjKK44X4FgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
